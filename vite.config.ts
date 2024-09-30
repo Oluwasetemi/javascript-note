9e43045c7cfd13db7439c402e724e9017c374ac9
oluwasetemi ojo <setemiojo@gmail.com>
2024-09-30 11:48:36 -0500

    refactor: Update package.json and vite.config.ts
    - Update the postinstall script in package.json to use npx instead of pnpm dlx
    - Add markdown-it-magic-link package to package.json
    - Create vite.config.ts file with slidev configuration and markdown-it-magic-link setup

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import MarkdownItMagicLink from 'markdown-it-magic-link'
+import { defineConfig } from 'vite'
+import '@slidev/cli'
+
+export default defineConfig({
+  slidev: {
+    markdown: {
+      markdownItSetup(md) {
+        md.use(MarkdownItMagicLink, {
+          linksMap: {
+            'NuxtLabs': { link: 'https://nuxtlabs.com', imageUrl: 'https://nuxtlabs.com/icon.png' },
+            'Vitest': 'https://github.com/vitest-dev/vitest',
+            'Slidev': 'https://github.com/slidevjs/slidev',
+            'UnoCSS': 'https://github.com/unocss/unocss',
+            'Vue': 'https://github.com/vuejs/core',
+            'Nuxt': 'https://github.com/nuxt/nuxt',
+            'Vite': 'https://github.com/vitejs/vite',
+            'ESLint': 'https://github.com/eslint/eslint',
+            'Netlify': { link: 'https://netlify.com', imageUrl: 'https://github.com/netlify.png' },
+            'Stackblitz': { link: 'https://stackblitz.com', imageUrl: 'https://github.com/stackblitz.png' },
+            'Vercel': { link: 'https://vercel.com', imageUrl: 'https://github.com/vercel.png' },
+          },
+        })
+      },
+    },
+  },
+})
